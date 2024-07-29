325c66b8ec1d199a5e64c7a886708b6ad7aa8075
Erik Pedersen <109049703+basementlivin@users.noreply.github.com>
2024-07-29 13:55:34 -0400

    Update Footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -9,10 +9,17 @@
     y-padding="sm"
   >
     <div
-      class="flex flex-wrap items-baseline justify-between gap-x-6 gap-y-3 leading-none"
+      class="flex flex-row justify-center gap-x-6 gap-y-3 leading-none"
     >
       <p>
-        &copy; {{ new Date().getFullYear() }} REVOLVE Gallery & Co-Op Studios. Site design and development by <a href="https://historyofsalad.com" target="_blank">History of Salad</a>.
+        &copy; {{ new Date().getFullYear() }} Spread the Jelly.
+        Site development by 
+        <a
+          href="https://historyofsalad.com"
+          target="_blank"
+        >
+          History of Salad.
+        </a>
       </p>
     </div>
   </Bounded>
