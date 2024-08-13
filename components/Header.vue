c1ce4618f22c06ce4c00a30565420af765dda71c
Erik Pedersen <109049703+basementlivin@users.noreply.github.com>
2024-08-12 22:22:18 -0400

    COMPONENT: Header / Menu

--- components/Header.vue
+++ components/Header.vue
@@ -1,39 +1,57 @@
 <script setup lang="ts">
 const navigation = useNavigation()
 const settings = useSettings()
+
+import { ref } from 'vue'
+
+import Logo from '@/assets/svg/logo--header.svg'
+import MenuHamburger from '@/assets/svg/menu--hamburger.svg'
+import MenuX from '@/assets/svg/menu--x.svg'
+
+const isMenuOpen = ref(false)
+
+const toggleMenu = () => {
+  isMenuOpen.value = !isMenuOpen.value
+}
 </script>
 
 <template>
-  <header>
-    <div
-      class=""
-    >
+  <header class="header">
+    <div class="header__inner">
       <NuxtLink
         to="/"
-        class=""
+        class="logo"
       >
-        {{ settings?.data.site_title }}
+        <Logo />
       </NuxtLink>
-      <nav>
-        <ul
-          class=""
-        >
+      <button 
+        class="menu-toggle" 
+        @click="toggleMenu"
+      >
+        <component :is="isMenuOpen ? MenuX : MenuHamburger" />
+      </button>
+    </div>
+    <nav
+      :class="{ 'menu-open': isMenuOpen }"
+      class="slide-in-menu"
+    >
+      <div class="slide-in-menu__inner">
+        <ul>
           <li
             v-for="item in navigation?.data.links"
             :key="$prismic.asText(item.label) || ''"
-            class=""
+            class="menu-item h4"
           >
-            <PrismicLink
-              :field="item.link"
-            >
-              {{
-                $prismic.asText(item.label)
-              }}
+            <PrismicLink :field="item.link">
+              {{ $prismic.asText(item.label) }}
             </PrismicLink>
           </li>
         </ul>
-      </nav>
-    </div>
+      </div>
+    </nav>
   </header>
 </template>
 
+<style lang="scss" scoped>
+  @import '@/assets/scss/components/_header.scss';
+</style>
