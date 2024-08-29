5dc8c27cc870705f61d02cc33cbbebaa8d11d3ae
Erik Pedersen <109049703+basementlivin@users.noreply.github.com>
2024-08-29 17:08:37 -0400

    Article Body style updates

--- app/prismic/richTextSerializer.ts
+++ app/prismic/richTextSerializer.ts
@@ -2,27 +2,27 @@ import { type HTMLRichTextMapSerializer } from '@prismicio/client';
 
 const serializer: HTMLRichTextMapSerializer = {
   heading1: ({ children }) =>
-    /* html */ `<h1 class="">${children}</h1>`,
+    /* html */ `<h1 class="h1">${children}</h1>`,
   heading2: ({ children }) =>
-    /* html */ `<h2 class="">${children}</h2>`,
+    /* html */ `<h2 class="h2">${children}</h2>`,
   heading3: ({ children }) =>
-    /* html */ `<h3 class="">${children}</h3>`,
+    /* html */ `<h3 class="h2">${children}</h3>`,
   paragraph: ({ children }) =>
-    /* html */ `<p class="">${children}</p>`,
+    /* html */ `<p class="body">${children}</p>`,
   oList: ({ children }) =>
-    /* html */ `<ol class="">${children}</ol>`,
+    /* html */ `<ol class="list">${children}</ol>`,
   oListItem: ({ children }) =>
-    /* html */ `<li class="">${children}</li>`,
+    /* html */ `<li class="item">${children}</li>`,
   list: ({ children }) =>
-    /* html */ `<ul class="">${children}</ul>`,
+    /* html */ `<ul class="list">${children}</ul>`,
   listItem: ({ children }) =>
-    /* html */ `<li class="">${children}</li>`,
+    /* html */ `<li class="item">${children}</li>`,
   preformatted: ({ children }) =>
     /* html */ `<pre class=""><code>${children}</code></pre>`,
   strong: ({ children }) =>
-    /* html */ `<strong class="">${children}</strong>`,
+    /* html */ `<strong class="body--bold">${children}</strong>`,
   hyperlink: ({ children, node }) =>
-    /* html */`<a href="${node.data.url}" class="">${children}</a>`
+    /* html */`<a href="${node.data.url}" class="link--copy">${children}</a>`
 }
 
 export default serializer;
