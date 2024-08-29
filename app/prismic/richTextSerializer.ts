06aeb0cf877e9942a4bc5f31dda4caae854c0061
Erik Pedersen <109049703+basementlivin@users.noreply.github.com>
2024-08-29 12:35:37 -0400

    Rm Tailwind classes from richTextSerializer

--- app/prismic/richTextSerializer.ts
+++ app/prismic/richTextSerializer.ts
@@ -2,27 +2,27 @@ import { type HTMLRichTextMapSerializer } from '@prismicio/client';
 
 const serializer: HTMLRichTextMapSerializer = {
   heading1: ({ children }) =>
-    /* html */ `<h1 class="font-semibold leading-tight tracking-tight md:leading-tight text-4xl md:text-5xl mb-7 mt-12 first:mt-0 last:mb-0">${children}</h1>`,
+    /* html */ `<h1 class="">${children}</h1>`,
   heading2: ({ children }) =>
-    /* html */ `<h2 class="font-semibold leading-tight tracking-tight md:leading-tight text-3xl md:text-4xl mb-7 mt-12 first:mt-0 last:mb-0">${children}</h2>`,
+    /* html */ `<h2 class="">${children}</h2>`,
   heading3: ({ children }) =>
-    /* html */ `<h3 class="font-semibold leading-tight tracking-tight md:leading-tight text-xl md:text-2xl mb-7 mt-12 first:mt-0 last:mb-0">${children}</h3>`,
+    /* html */ `<h3 class="">${children}</h3>`,
   paragraph: ({ children }) =>
-    /* html */ `<p class="mb-7 last:mb-0">${children}</p>`,
+    /* html */ `<p class="">${children}</p>`,
   oList: ({ children }) =>
-    /* html */ `<ol class="mb-7 pl-4 last:mb-0 md:pl-6">${children}</ol>`,
+    /* html */ `<ol class="">${children}</ol>`,
   oListItem: ({ children }) =>
-    /* html */ `<li class="mb-1 list-decimal pl-1 last:mb-0 md:pl-2">${children}</li>`,
+    /* html */ `<li class="">${children}</li>`,
   list: ({ children }) =>
-    /* html */ `<ul class="mb-7 pl-4 last:mb-0 md:pl-6">${children}</ul>`,
+    /* html */ `<ul class="">${children}</ul>`,
   listItem: ({ children }) =>
-    /* html */ `<li class="mb-1 list-disc pl-1 last:mb-0 md:pl-2">${children}</li>`,
+    /* html */ `<li class="">${children}</li>`,
   preformatted: ({ children }) =>
-    /* html */ `<pre class="mb-7 rounded bg-slate-100 p-4 text-sm last:mb-0 md:p-8 md:text-lg"><code>${children}</code></pre>`,
+    /* html */ `<pre class=""><code>${children}</code></pre>`,
   strong: ({ children }) =>
-    /* html */ `<strong class="font-semibold">${children}</strong>`,
+    /* html */ `<strong class="">${children}</strong>`,
   hyperlink: ({ children, node }) =>
-    /* html */`<a href="${node.data.url}" class="underline decoration-1 underline-offset-2">${children}</a>`
+    /* html */`<a href="${node.data.url}" class="">${children}</a>`
 }
 
 export default serializer;
